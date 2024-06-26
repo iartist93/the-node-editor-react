595c6ff461dd5a5a64899589602696874b732731
ahmed ayman <mongez.me@gmail.com>
2024-06-26 19:47:04 +0300

    fix: input range thumb color

--- src/app/components/connection/Connection.tsx
+++ src/app/components/connection/Connection.tsx
@@ -1,114 +1,116 @@
-import { useEffect, useState } from "react";
-import { useStore } from "@/app/store";
-import { useMouse } from "@uidotdev/usehooks";
+import {useEffect, useState} from "react";
+import {useStore} from "@/app/store";
+import {useMouse} from "@uidotdev/usehooks";
 import {
-  isSamePosition,
-  getPath,
-  getSocketPosition,
-  getTransformedPosition,
+    isSamePosition,
+    getPath,
+    getSocketPosition,
+    getTransformedPosition,
 } from "@/app/components/connection/utils";
-import { ConnectionData, Position } from "@/app/components/node/utils";
+import {ConnectionData, Position} from "@/app/components/node/utils";
 import _ from "lodash";
 
 export function Connection({
-  id,
-  outputNodeId,
-  outputSocketId,
-  inputNodeId,
-  inputSocketId,
-}: ConnectionData) {
-  const [mouse] = useMouse();
-  const [outputPosition, setOutputPosition] = useState<Position>({
-    x: mouse.x,
-    y: mouse.y,
-  });
-  const [inputPosition, setInputPosition] = useState<Position>({
-    x: mouse.x,
-    y: mouse.y,
-  });
-  const [path, setPath] = useState("");
+                               id,
+                               outputNodeId,
+                               outputSocketId,
+                               inputNodeId,
+                               inputSocketId,
+                           }: ConnectionData) {
+    const [mouse] = useMouse();
+    const [outputPosition, setOutputPosition] = useState<Position>({
+        x: mouse.x,
+        y: mouse.y,
+    });
+    const [inputPosition, setInputPosition] = useState<Position>({
+        x: mouse.x,
+        y: mouse.y,
+    });
+    const [path, setPath] = useState("");
 
-  const editorScale = useStore((store) => store.editorScale);
-  const findNode = useStore((store) => store.findNode);
+    const editorScale = useStore((store) => store.editorScale);
+    const findNode = useStore((store) => store.findNode);
 
-  const inputNode = findNode(inputNodeId);
-  const outputNode = findNode(outputNodeId);
+    const inputNode = findNode(inputNodeId);
+    const outputNode = findNode(outputNodeId);
 
-  const updatePosition = () => {
-    let sourcePosition: Position | null = outputSocketId
-      ? getSocketPosition(outputSocketId, editorScale)
-      : null;
-    let targetPosition: Position | null = inputSocketId
-      ? getSocketPosition(inputSocketId, editorScale)
-      : null;
+    const updatePosition = () => {
+        let sourcePosition: Position | null = outputSocketId
+            ? getSocketPosition(outputSocketId, editorScale)
+            : null;
+        let targetPosition: Position | null = inputSocketId
+            ? getSocketPosition(inputSocketId, editorScale)
+            : null;
 
-    const transformedPosition = getTransformedPosition(
-      { x: mouse.x, y: mouse.y },
-      editorScale,
+        const transformedPosition = getTransformedPosition(
+            {x: mouse.x, y: mouse.y},
+            editorScale,
+        );
+
+        if (!targetPosition) {
+            if (
+                transformedPosition.x === 0 &&
+                transformedPosition.y === 0 &&
+                sourcePosition
+            ) {
+                targetPosition = {x: sourcePosition.x, y: sourcePosition.y};
+            } else {
+                targetPosition = {x: transformedPosition.x, y: transformedPosition.y};
+            }
+        } else if (!sourcePosition) {
+            if (
+                transformedPosition.x === 0 &&
+                transformedPosition.y === 0 &&
+                targetPosition
+            ) {
+                sourcePosition = {x: targetPosition.x, y: targetPosition.y};
+            } else {
+                sourcePosition = {x: transformedPosition.x, y: transformedPosition.y};
+            }
+        }
+
+        if (sourcePosition && !isSamePosition(outputPosition, sourcePosition)) {
+            setOutputPosition(sourcePosition);
+        }
+
+        if (targetPosition && !isSamePosition(inputPosition, targetPosition)) {
+            setInputPosition(targetPosition);
+        }
+    };
+
+    const updatePath = () => {
+        console.log("updatePath > output ", outputPosition)
+        console.log("updatePath > input ", inputPosition)
+        const p = getPath(outputPosition, inputPosition);
+        setPath(p);
+    };
+
+    const throttleUpdatePosition = _.throttle(updatePosition, 100);
+    const throttleUpdatePath = _.throttle(updatePath, 100);
+
+    useEffect(() => {
+        throttleUpdatePosition();
+    }, [
+        outputNodeId,
+        outputSocketId,
+        inputNodeId,
+        inputSocketId,
+        mouse,
+        inputNode,
+        outputNode,
+    ]);
+
+    useEffect(() => {
+        throttleUpdatePath();
+    }, [outputPosition, inputPosition]);
+
+    return (
+        <path
+            d={path}
+            stroke="#489"
+            strokeWidth="3"
+            fill="none"
+            strokeLinecap="round"
+        />
     );
-
-    if (!targetPosition) {
-      if (
-        transformedPosition.x === 0 &&
-        transformedPosition.y === 0 &&
-        sourcePosition
-      ) {
-        targetPosition = { x: sourcePosition.x, y: sourcePosition.y };
-      } else {
-        targetPosition = { x: transformedPosition.x, y: transformedPosition.y };
-      }
-    } else if (!sourcePosition) {
-      if (
-        transformedPosition.x === 0 &&
-        transformedPosition.y === 0 &&
-        targetPosition
-      ) {
-        sourcePosition = { x: targetPosition.x, y: targetPosition.y };
-      } else {
-        sourcePosition = { x: transformedPosition.x, y: transformedPosition.y };
-      }
-    }
-
-    if (sourcePosition && !isSamePosition(outputPosition, sourcePosition)) {
-      setOutputPosition(sourcePosition);
-    }
-
-    if (targetPosition && !isSamePosition(inputPosition, targetPosition)) {
-      setInputPosition(targetPosition);
-    }
-  };
-
-  const updatePath = () => {
-    const p = getPath(outputPosition, inputPosition);
-    setPath(p);
-  };
-
-  const throttleUpdatePosition = _.throttle(updatePosition, 100);
-  const throttleUpdatePath = _.throttle(updatePath, 100);
-
-  useEffect(() => {
-    throttleUpdatePosition();
-  }, [
-    outputNodeId,
-    outputSocketId,
-    inputNodeId,
-    inputSocketId,
-    mouse,
-    inputNode,
-    outputNode,
-  ]);
-
-  useEffect(() => {
-    throttleUpdatePath();
-  }, [outputPosition, inputPosition]);
-
-  return (
-    <path
-      d={path}
-      stroke="#489"
-      strokeWidth="3"
-      fill="none"
-      strokeLinecap="round"
-    />
-  );
 }
--- src/app/components/socket/RowColorSocket.tsx
+++ src/app/components/socket/RowColorSocket.tsx
@@ -1,49 +1,49 @@
 import "./styles.css";
-import React, { useState } from "react";
-import { Socket } from "@/app/components/socket/Socket";
+import React, {useState} from "react";
+import {Socket} from "@/app/components/socket/Socket";
 import cc from "classcat";
 
 type ColorSocketProps = {
-  id: string;
-  nodeId: string;
-  value: string;
-  name: string;
-  variant: string;
-  onChange?: (value: string) => void;
+    id: string;
+    nodeId: string;
+    value: string;
+    name: string;
+    variant: string;
+    onChange?: (value: string) => void;
 };
 
 export function RowColorSocket({
-  id,
-  nodeId,
-  value,
-  name,
-  onChange,
-}: ColorSocketProps) {
-  const [color, setColor] = useState(value);
+                                   id,
+                                   nodeId,
+                                   value,
+                                   name,
+                                   onChange,
+                               }: ColorSocketProps) {
+    const [color, setColor] = useState(value);
 
-  const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setColor(e.target.value);
-    if (onChange) {
-      onChange(e.target.value);
-    }
-  };
+    const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setColor(e.target.value);
+        if (onChange) {
+            onChange(e.target.value);
+        }
+    };
 
-  return (
-    <div className="no-drag flex justify-between items-center relative px-4 my-2 h-6">
-      <div className="select-none pointer-events-none text-xs text-black font-bold">
-        <p>{name}</p>
-      </div>
+    return (
+        <div className="no-drag flex justify-between items-center relative px-4 my-2 h-6">
+            <div className="select-none pointer-events-none text-xs text-black font-bold">
+                <p>{name}</p>
+            </div>
 
-      <div className="relative h-full">
-        <input
-          type="color"
-          value={color}
-          onChange={handleColorChange}
-          className="w-32 h-full"
-        />
-        <p className="text-white text-sm absolute right-2 top-1">{color}</p>
-      </div>
-      <Socket id={id} nodeId={nodeId} type={"input"} datatype={"color"} />
-    </div>
-  );
+            <div className="relative h-full">
+                <input
+                    type="color"
+                    value={color}
+                    onChange={handleColorChange}
+                    className="w-32 h-full"
+                />
+                <p className="text-white text-xs absolute right-2 top-1">{color}</p>
+            </div>
+            <Socket id={id} nodeId={nodeId} type={"input"} datatype={"color"}/>
+        </div>
+    );
 }
